7ef9f51ef6bfccbcf2b3c4d06817769ad44ace8b
Lexuanduy <lexuanduy07@gmail.com>
2018-12-09 22:43:52 +0700

    create

---
+++ WAD_Exam_2/Data/WADExam2Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WAD_Exam_2.Models;
+
+namespace WAD_Exam_2.Data
+{
+    public class WADExam2Context: DbContext
+    { 
+        public WADExam2Context(DbContextOptions<WADExam2Context> options)
+            : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
